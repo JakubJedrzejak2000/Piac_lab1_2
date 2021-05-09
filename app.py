e5a96e2d307757142cf3dd42d1c43d4b129ce3e0
JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>
2021-05-09 14:54:19 +0200

    My first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/aboutme")
+def aboutme():
+    return render_template("aboutme.html")
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template('gallery.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
